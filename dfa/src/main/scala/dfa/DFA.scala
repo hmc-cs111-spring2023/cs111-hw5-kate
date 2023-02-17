11a086e511c63050d2f26b91bb1ff6c12b376e00
Kate Phillips <47925526+muffinkate@users.noreply.github.com>
2023-02-17 06:51:17 +0000

    working on accepts method

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -3,7 +3,15 @@ package dfa // leave this line in the file
 case class State(label: String)
 case class Transition(from: State, symbol: Char, to: State)
 
-class DFA(val state: Set[State], val startState: State, val acceptingStates: Set[State]):
+class DFA(val states: Set[State], val startState: State, val acceptingStates: Set[State]):
     def accepts(input: String): Boolean = 
-        True
+        var currentState = startState
+        //for (var x <- input.length()) {
+        //    var newState = something with transition and current state and input[x]
+
+        //}
+
+        if acceptingStates.exists(currentState) then true
+        else false
+
     
