07773c967edbf5aa59f7f03f97bf4964aa54af73
Kate Phillips <47925526+muffinkate@users.noreply.github.com>
2023-02-17 21:04:24 +0000

    implementing accepts

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -6,10 +6,10 @@ case class Transition(from: State, to: State, symbol: Char)
 class DFA(val states: Set[State], val start: State, val accept: Set[State], val transitions: Set[Transition]):
     def accepts(input: String): Boolean = 
         var currentState = start
-        //for (var x <- input.length()) {
-        //    var newState = something with transition and current state and input[x]
-
-        //}
+        for (x <- 0 to input.length()) {
+            val t = transitions.filter(_.from == currentState).filter(_.symbol == input.charAt(x)).head
+            currentState = t.to
+        }
 
         if accept.contains(currentState) then true
         else false
