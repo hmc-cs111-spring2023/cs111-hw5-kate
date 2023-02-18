fe8338e36b732942782aeadcaa75c74d55fa3c99
Kate Phillips <47925526+muffinkate@users.noreply.github.com>
2023-02-18 08:52:16 +0000

    implementing nullable and derivative

--- regex/src/main/scala/regex/Regex.scala
+++ regex/src/main/scala/regex/Regex.scala
@@ -37,10 +37,26 @@ def simplify(lang: RegularLanguage): RegularLanguage = lang match
   case _ => lang
 
 /** A language is nullable if it contains ε */
-def nullable(lang: RegularLanguage): Boolean = ???
+def nullable(lang: RegularLanguage): Boolean = lang match
+  case Empty => false
+  case Epsilon => true
+  case Character(_) => false
+  case Concat(re1, re2) => nullable(re1) && nullable(re2)
+  case Union(re1, re2) => nullable(re1) || nullable(re2)
+  case Star(_) => true
+  case _ => false
 
 /** Computes the derivative of a language, with respect to a character */
-def derivative(l: RegularLanguage)(c: Char): RegularLanguage = ???
+def derivative(l: RegularLanguage)(c: Char): RegularLanguage = l match
+  case Empty => Empty
+  case Epsilon => Empty
+  case Character(d) if d == c => Epsilon
+  case Character(_) => Empty
+  case Union(re1 ,re2) =>  Union(derivative(re1)(c), derivative(re2)(c))
+  case Concat(re1, re2) if !(nullable(re1)) => Concat(derivative(re1)(c), re2)
+  case Concat(re1, re2) => Union(Concat(derivative(re1)(2), re2),derivative(re2)(c))
+  case Star(re) => Concat(derivative(re)(c), Star(re))
+
 
 /** *****************************************************************************
   * String-matching with regular expressions
